ddbc3c42114442a723986c4eb0257869eb2248ea
Smoliarenko Alex <smoliarenko.sasha@gmail.com>
2024-09-18 18:13:12 +0300

    fix

--- src/js/books.js
+++ src/js/books.js
@@ -1,63 +1,63 @@
 const booksList = document.querySelector("#books_list");
 export const books = [
     {
-        image: "images/game-of-thrones.jpeg",
+        image: "&quot;game-of-thrones.b4d417b9.jpeg&quot",
         bookName: "Game of Thrones",
         author: "Jorge R. R Martin",
         year: 1996,
         rating: 4.4,
     },
     {
-        image: "images/harry-potter.jpeg",
+        image: "&quot;harry-potter.2b18bf2e.jpeg&quot",
         bookName: "Harry Potter and philosopher's stone",
         author: "J. K. Rowling",
         year: 1997,
         rating: 4.5,
     },
     {
-        image: "images/lord-of-the-rings.jpeg",
+        image: "&quot;lord-of-the-rings.e24c3c67.jpeg&quot",
         bookName: "Lord of the rings",
         author: "J. R. R. Tolkien",
         year: 1967,
         rating: 4.5,
     },
     {
-        image: "images/hobbit.jpeg",
+        image: "&quot;hobbit.cac4f096.jpeg&quot",
         bookName: "The Hobbit",
         author: "J. R. R. Tolkien",
         year: 1937,
         rating: 4.3,
     },
     {
-        image: "images/at-the-west-front.jpg",
+        image: "&quot;at-the-west-front.b2d1c9ce.jpg&quot",
         bookName: "Nothing new on the east front",
         author: "Erich Maria Remarque",
         year: 1929,
         rating: 4.5,
     },
     {
-        image: "images/fahrenheit-451.jpg",
+        image: "&quot;fahrenheit-451.a4f69e9b.jpg&quot",
         bookName: "Fahrenheit 451",
         author: "Ray Bradbury",
         year: 1953,
         rating: 4.7,
     },
     {
-        image: "images/little-prince.jpg",
+        image: "&quot;little-prince.8d1addbb.jpg&quot",
         bookName: "The little prince",
         author: "Antoine de Saint-Exupéry",
         year: 1943,
         rating: 4.9,
     },
     {
-        image: "images/narnia.jpg",
+        image: "&quot;narnia.7a4db71f.jpg&quot",
         bookName: "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe",
         author: "C.S. Lewis",
         year: 1950,
         rating: 4.3,
     },
     {
-        image: "images/harry-potter-2.jpg",
+        image: "&quot;harry-potter-2.57f49c7d.jpg&quot",
         bookName: "Harry Potter and the Chamber of Secrets",
         author: "J.K Rawling",
         year: 1998,
@@ -73,7 +73,7 @@ books.forEach(book => {
                 <p class="books_list_rating_text">${book.rating}</p> 
                 <svg width="23" height="23" class="books_rating_img">
       <use
-        href="images/symbol-defs.svg#icon-star"
+        href="/symbol-defs.a8b2e413.svg#icon-star"
         width="24"
         height="24"
       ></use>
